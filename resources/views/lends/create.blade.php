c7999992dc488cda99245326dd0f0dc2f89e019f
shinnosuke.yata <shinnosuke.yata@rakuten.com>
2018-07-25 05:04:57 +0000

    finish!!!!!!!!

--- resources/views/lends/create.blade.php
+++ resources/views/lends/create.blade.php
@@ -1,37 +1,32 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <div class="page-header">
-        <h1>Create lend date</h1>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<div class="row">
+    <div class="col-md-12">
+        <form action="{{ route('lends.store') }}" method="POST">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="hidden" name="id" value="{{ $id}}">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <label for="start" >START</label>
+                    <input type="text" name="start" id="datepicker-start" autocomplete="off">
+                    <label for="end">END</label>
+                    <input type="text" name="end" id="datepicker-end" autocomplete="off">
+                    <select name="type">
+                        <option value="">受け渡し方法</option>
+                        <option value="rocker">ロッカーで受け渡し</option>
+                        <option value="delivery">配達する</option>
+                    </select> 
+                    <button class="btn btn-primary" type="submit" >Create</button>
+                </div>
+            </div>
+        </form>
     </div>
+</div>
+<script type="text/javascript">
+$('#datepicker-start').datepicker({ dateFormat: "yy-mm-dd" }).val();
+$('#datepicker-end').datepicker({ dateFormat: "yy-mm-dd" }).val();
+</script>
 
-
-    <div class="row">
-        <div class="col-md-12">
-                
-            <form action="{{ route('lends.store') }}" method="POST">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="id" value="{{ $id}}">
-                <div class="row">
-                <div class="form-group col-xs-2">
-                    <label for="start">START</label>
-                   <input type="text" name="start" id="datepicker-start" autocomplete="off">
-                </div>
-                <div class="form-group col-xs-offset-1 col-xs-2">
-                     <label for="end">END</label>
-                     <input type="text" name="end" id="datepicker-end" autocomplete="off">
-                </div>
-                </div>
-            <a class="btn btn-default" href="{{ route('lends.show',['id'=>$id]) }}">Back</a>
-            <button class="btn btn-primary" type="submit" >Create</button>
-            </form>
-        </div>
-    </div>
-    <script type="text/javascript">
-    $('#datepicker-start').datepicker({ dateFormat: "yy-mm-dd" }).val();
-    $('#datepicker-end').datepicker({ dateFormat: "yy-mm-dd" }).val();
-    </script>
-
-
-@endsection
